7bc27bc473e5e9651409f7aa02c3ee2129941dfd
SergioLucasINT <99209004+SergioLucasINT@users.noreply.github.com>
2022-02-18 13:42:53 -0300

    Semana 2
    Alterações:
    GDD atualizado, versão 0.2
    GDDs antigos e Modelo GDD arquivado na pasta "Antigos"
    
    Atualização Semana 2
    
    Documentos GDD alterados de Word para PDF
    
    Atualização Semana 2, 2
    
    Removido documentos .docx
    
    Comentários nos códigos
    
    Scripts comentados
    
    Pastas Arrumadas
    
    Adição da pasta de Assets e organização dos arquivos

--- src/project01/Cenas/Cenas de IU/EscolhaPoder.gd
+++ src/project01/Cenas/Cenas de IU/EscolhaPoder.gd
@@ -3,16 +3,16 @@ extends Control
 func _ready():
 	pass # Replace with function body.
 
-
-
-
 func _on_PL_pressed():
-	get_tree().change_scene("res://Cenas/Cenas Principais/Legislativo.tscn")
+	get_tree().change_scene("res://Cenas/Cenas Principais/Legislativo.tscn") 
+	#Escolhe o poder Legislativo
 
 
 func _on_PE_pressed():
-	get_tree().change_scene("res://Cenas/Cenas Principais/Executivo.tscn")
+	get_tree().change_scene("res://Cenas/Cenas Principais/Executivo.tscn") 
+	#Escolhe o poder Executivo
 
 
 func _on_PJ_pressed():
-	get_tree().change_scene("res://Cenas/Cenas Principais/Jurídico.tscn")
+	get_tree().change_scene("res://Cenas/Cenas Principais/Jurídico.tscn") 
+	#Escolhe o poder Judicial

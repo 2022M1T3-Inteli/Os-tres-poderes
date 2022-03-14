cf9d53455ab0989ebbc21438deb4dc1393ad41fa
EricTach <99208930+EricTach@users.noreply.github.com>
2022-03-14 14:57:09 -0300

    vinicius
    vininini

---
+++ src/project01/Cenas/Cenas Principais/Legislativo.gd
@@ -0,0 +1,7 @@
+extends Control
+
+func _ready():
+	pass
+
+func _on_Button5_pressed():
+	get_tree().change_scene("res://Cenas/Cenas de IU/EscolhaPoder.tscn") #Botão de voltar
---
+++ src/project01/Cenas/Cenas de IU/MainMenu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _ready():
+	OS.window_fullscreen = true
+
+func _on_Button3_pressed():
+	get_tree().quit() #Sai do jogo
+
+
+func _on_Start_pressed():
+	get_tree().change_scene("res://Cenas/Cenas de IU/EscolhaPoder.tscn") #Começa o Jogo

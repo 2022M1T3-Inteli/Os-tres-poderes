51ac32c40f83baba119284435e6d1abeb8e87ef8
EricTach <99208930+EricTach@users.noreply.github.com>
2022-03-14 14:54:42 -0300

    show
    apagi

--- src/project01/Scripts/Sinais.gd
+++
@@ -1,7 +0,0 @@
-## Este Script é um "singleton", que é configurado indo em "Project > Project Settings > AutoLoad". um singleton é um script que roda a todo o tempo no jogo, e não precisa ser atrelado a um nó específico. Extremamente útil para manter informações do jogo (Data Sheet) e armazenar dados ou sinais. 
-
-extends Node2D
-
-signal morreu
-signal recompensar
-signal pontuar

c9bc0bb1d6f51f50f09c53ac9cbbcba3f691bd3e
EricTach <99208930+EricTach@users.noreply.github.com>
2022-03-22 14:18:55 -0300

    Judiciario
    Adição/ arrumei o sistema judiciário do Brasil

--- src/project01/Scripts/Scripts das Interfaces/Jurídico.gd
+++ src/project01/Scripts/Scripts das Interfaces/Jurídico.gd
@@ -5,3 +5,8 @@ func _ready():
 
 func _on_Button2_pressed():
 	get_tree().change_scene("res://Cenas/Interface do Usuário/EscolhaPoder.tscn") #Botão de voltar
+
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://Cenas/Poderes/Juridico/Botões Jurídico/Pergunta1.tscn") 
+		#altera para a cena onde será mostrado os dados do minigame

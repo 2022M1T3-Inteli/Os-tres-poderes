b8f693ce9f013e11b05ed02f1f7bcff95c6edfd4
EricTach <99208930+EricTach@users.noreply.github.com>
2022-03-24 11:11:27 -0300

    Cenas de Dados
    Finalização das cenas de dados e design

---
+++ src/project01/Cenas/Poderes/Juridico/Botões Jurídico/Pergunta1.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_DadosJogador_pressed():
+	get_tree().change_scene("res://Cenas/Poderes/Juridico/Botões Jurídico/DadosJogadores.tscn")
+
+
+func _on_DadosMinigame_pressed():
+	get_tree().change_scene("res://Cenas/Poderes/Juridico/Botões Jurídico/DadosMinigame.tscn")
+
+
+func _on_DadosDebate_pressed():
+	get_tree().change_scene("res://Cenas/Poderes/Juridico/Botões Jurídico/DadosDebate.tscn")

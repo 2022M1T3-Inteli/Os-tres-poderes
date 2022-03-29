1c2f2963a6322dc6f780c6aa40da5a09aed9b644
SergioLucasINT <99209004+SergioLucasINT@users.noreply.github.com>
2022-03-29 11:29:08 -0300

    Update 01
    Mudanças do Sergio Implementadas

--- src/project01/Scripts/Scripts Globais/Global.gd
+++ src/project01/Scripts/Scripts Globais/Global.gd
@@ -2,6 +2,7 @@ extends Node
 
 var ideia = false
 var etapa2 = false
+var poder_escolhido = ""
 
 var pontosminigame = 0.0
 
--- src/project01/Scripts/Scripts das Interfaces/EscolhaPoder.gd
+++ src/project01/Scripts/Scripts das Interfaces/EscolhaPoder.gd
@@ -1,18 +1,48 @@
 extends Control
 
-func _on_PE_pressed():
-	get_tree().change_scene("res://Cenas/Poderes/Executivo/Executivo.tscn") 
-	#Escolhe o poder Executivo
+onready var PL = $"Poder Legislativo"
+onready var PJ = $"Poder Judiciário"
+onready var PS = $"Poder Selecionado"
 
-func _on_PL_pressed():
-	if Global.etapa2:
-		get_tree().change_scene("res://Cenas/Poderes/Legislativo/Debates/Debate.tscn") 
-		#Escolhe o poder Legislativo
-
-func _on_PJ_pressed():
-	if Global.etapa2:
-		get_tree().change_scene("res://Cenas/Poderes/Juridico/Jurídico.tscn") 
-		#Escolhe o poder Jurídico 
+func _ready():
+	if Global.etapa2: 
+		PL.disabled = false
+		PJ.disabled = false
+	PS.hide()
 
 func _on_voltar_pressed():
 	get_tree().change_scene("res://Cenas/Interface do Usuário/MainMenu.tscn")
+
+
+func _on_Poder_Judicirio_pressed():
+	get_tree().change_scene("res://Cenas/Poderes/Juridico/Jurídico.tscn") 
+
+
+func _on_Poder_Executivo_pressed():
+	get_tree().change_scene("res://Cenas/Poderes/Executivo/Executivo.tscn") 
+	#Escolhe o poder Executivo
+
+
+func _on_Poder_Legislativo_pressed():
+		get_tree().change_scene("res://Cenas/Poderes/Legislativo/Debates/Debate.tscn") 
+		#Escolhe o poder Legislativo
+
+
+func _on_Poder_Executivo_mouse_entered():
+	Global.poder_escolhido = "Poder Executivo"
+	PS.show()
+	PS.rect_position.x = 197
+	PS.rect_position.y = 275
+
+func _on_Poder_Judicirio_mouse_entered():
+	Global.poder_escolhido = "Poder Judiciário"
+	PS.show()
+	PS.rect_position.x = 1112
+	PS.rect_position.y = 191
+
+
+func _on_Poder_Legislativo_mouse_entered():
+	Global.poder_escolhido = "Poder Legislativo"
+	PS.show()
+	PS.rect_position.x = 587
+	PS.rect_position.y = 106

0d7d57ebc68eacc5c26ce051203e6d827201f1f4
SergioLucasINT <99209004+SergioLucasINT@users.noreply.github.com>
2022-04-13 19:45:06 -0300

    guto_atualizações
    guto

--- src/project01/Scripts/Scripts das Interfaces/MenuDados.gd
+++ src/project01/Scripts/Scripts das Interfaces/MenuDados.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 onready var pc = $Background
 onready var botoes = $"Opções"
+onready var jornal = $Jornal
 var pcligado = load("res://Recursos/Planos de Fundo/fundo-torto(1).jpg")
 var pcdesligado = load("res://Recursos/Planos de Fundo/PCDesligado.jpg")
 
@@ -20,6 +21,12 @@ func _on_DadosDebate_pressed():
 func _on_Voltar_pressed():
 	get_tree().change_scene("res://Cenas/Poderes/Judiciário/Judiciário.tscn")
 
+func _on_Noticias_pressed():
+	get_tree().change_scene("res://Cenas/Poderes/Judiciário/Opções/Jornal.tscn")
+
+
+
+
 func _on_Power_pressed():
 	if pc.texture == pcligado:
 		pc.texture = pcdesligado
@@ -27,3 +34,10 @@ func _on_Power_pressed():
 	else:
 		pc.texture = pcligado
 		botoes.show()
+
+
+
+
+
+
+
